c91188d4f9c5d0d26cc259091f1cde01e6eeacf2
bhaveshsolanki159-codex <bhaveshsolankibj159@gmail.com>
2025-02-14 09:48:23 +0530

    done half

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 class Library:
+    TotalBooks=0
     def __init__(self):
         self.books =[]
     
     def add_book(self,book):
+        # no0fBooks+=1
+        self.TotalBooks+=1
         self.books.append(book)
+
+    # def TotalBooks(self):
+    #     return no0fBooks
 
     def display_books(self):
         for book in self.books:
@@ -15,9 +21,47 @@ class Library:
     def remove_book(self,book):
         self.books.remove(book)
 
+# class ScienceFrictionBooks(Library):
+#     def __init__(self):
+#         super().__init__()
+#         self.ScienceFrictionBooks = []
+
+#     def add_book(self, book):
+#         self.books.append(book)
+#         return super().add_book(book)
+
+#     def display_books(self):
+#         return super().display_books()
+    
+#     def no0fBooks(self):
+#         return super().no0fBooks()
+    
+#     def remove_book(self, book):
+#         return super().remove_book(book)
+
+
+
 if __name__ == "__main__":
-    library = Library()
-    library.add_book("Book1")
-    library.add_book("Book2")
-    library.display_books()
-    print(library.no0fBooks())
+    ScienceFrictionBooks = Library()
+
+    ScienceFrictionBooks.add_book("Physics")
+    ScienceFrictionBooks.add_book("Chemistry")
+    ScienceFrictionBooks.add_book("Biology")
+    ScienceFrictionBooks.add_book("Mathematics")
+
+    ScienceFrictionBooks.display_books()
+    print(ScienceFrictionBooks.no0fBooks())
+
+    PhylosophyBooks = Library()
+
+    PhylosophyBooks.add_book("The Republic")
+    PhylosophyBooks.add_book("The Prince")
+    PhylosophyBooks.add_book("The Art of War")
+    PhylosophyBooks.add_book("The Wealth of Nations")
+
+    PhylosophyBooks.display_books()
+    print(PhylosophyBooks.no0fBooks())
+
+    print(Library.Books())
+
+    
